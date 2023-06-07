19cda4f6c66427494eec8bbb6c5096c5a332020a
Whitney <whitneydawson123@gmail.com>
2023-06-07 11:43:57 -0400

    initial setup

---
+++ src/components/Menuitem/Menultem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menultem = () => {
+  return (
+    <div>Menultem</div>
+  )
+}
+
+export default Menultem
---
+++ src/container/Menu/SpecialMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialMenu = () => {
+  return (
+    <div>SpecialMenu</div>
+  )
+}
+
+export default SpecialMenu
