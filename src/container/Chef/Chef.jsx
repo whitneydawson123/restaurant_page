a1707fc2d78872a2f109644e0d1c1d93ab34a3a4
Whitney <whitneydawson123@gmail.com>
2023-06-07 22:37:50 -0400

    finished chef section and fixed bug

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+import './Chef.css'
 
 const Chef = () => {
   return (
-    <div>Chef</div>
+    <div className='app__bg app__wrapper section__padding'>
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+        <img src={images.chef} alt='chef_image' />
+      </div>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chef's word" />
+        <h1 className='headtext__cormorant'>What we believe in</h1>
+
+        <div className='app__chef-content'>
+          <div className='app__chef-content_quote'>
+            <img src={images.quote} alt="quote_image" />
+            <p className='p__opensans'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <p className='p__opensans'>Voluptatum eius cumque consectetur cupiditate iusto ipsum ea odit alias maiores qui doloremque odio fugiat perferendis ipsa beatae, tempore placeat! Assumenda, porro.</p>
+        </div>
+
+        <div className='app__chef-sign'>
+          <p>Kevin Luo</p>
+          <p className='p__opensans'>Chef & Founder</p>
+          <img src={images.sign} alt="sign_image" />
+        </div>
+      </div>
+    </div>
   )
 }
 
